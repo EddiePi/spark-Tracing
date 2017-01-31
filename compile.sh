80d10ac9bed620ccf40b11f00a44b1bf7e44b085
EddiePi <piyidi36@sina.com>
2017-01-31 15:17:22 -0700

    thrift: add/remove metrics

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 export MAVEN_OPTS="-Xmx2g -XX:ReservedCodeCacheSize=512m -XX:MaxPermSize=512m";
-mvn -Pyarn -Phadoop-2.7 -Dhadoop.version=2.7.3 -DskipTests clean package -e;
+mvn -Pyarn -Phadoop-2.7 -Dhadoop.version=2.7.3 -DskipTests clean package -pl core -amd -e;
+#mvn -Pyarn -Phadoop-2.7 -Dhadoop.version=2.7.3 -DskipTests clean package -e;
 
